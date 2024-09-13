a7049216da38234d18aec2582525ed7faa4e31dc
SubhamBurman <105504422+SubhamBurman@users.noreply.github.com>
2024-09-13 17:25:25 +0000

    creat responsive imsraniganj project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./*.js"],
+  theme: {
+    extend: {
+      animation: {
+        "loop-scroll": "loop-scroll 100s linear infinite"
+      },
+      keyframes: {
+        "loop-scroll":{
+          from: {transform: "translateX(0)"},
+          to: {transform: "translateX(-100%)"}
+
+        },
+
+      },
+    },
+  },
+  plugins: [],
+}
+
