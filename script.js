ccfd2787fc468ce64924f0aa3efcf9564ff9e307
SubhamBurman <105504422+SubhamBurman@users.noreply.github.com>
2024-09-15 13:46:19 +0000

    contact bug fix

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// Initialize EmailJS with your public key
-emailjs.init('UJch24NZeaqlgbFGE'); // Replace with your actual public key from EmailJS
-
-// Handle form submission
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent default form submission
-
-  // Send the form data using EmailJS
-  emailjs.sendForm('service_s1rz41j', 'template_rgayev6', this)
-    .then(function(response) {
-      document.getElementById('response').innerText = 'Message sent successfully!';
-      document.getElementById('contact-form').reset(); // Reset the form
-    }, function(error) {
-      document.getElementById('response').innerText = 'Failed to send message. Please try again later.';
-      console.error('Error:', error);
-    });
-});
-
-  
-
-
-
-
-
 const navdialog = document.getElementById("nav-dialog")
 
 function handleMenu(){
@@ -29,11 +5,6 @@ function handleMenu(){
 }
 
 
-var typed = new Typed("#auto-type", {
-  strings: ["	Basic Ayurvedic System of Medicines", "	Diploma in Child Health Care", "	Bachelor in Dental Alternative Science", "Basic Nursing Training", "	Diploma in Female Nursing"],
-  typeSpeed: 50,
-  backSpeed: 50,
-  loop:true
-})
+
 
 
